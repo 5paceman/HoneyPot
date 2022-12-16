30723aa555e4985c18e017eacbf46d6fbd30d832
5paceman <tom.james15@hotmail.com>
2022-12-16 00:05:29 +0000

    Added app icons to process window and fixed pid selection

--- wxProcessListCtrl.cpp
+++ wxProcessListCtrl.cpp
@@ -1,28 +1,60 @@
 #include "wxProcessListCtrl.h"
 #include "../nightshade/Utils.h"
 
-wxProcessListCtrl::wxProcessListCtrl(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, wxTextCtrl* searchBox) : wxListCtrl(parent, id, pos, size, wxLC_REPORT | wxLC_VRULES | wxLC_SINGLE_SEL | wxLC_VIRTUAL)
+wxProcessListCtrl::wxProcessListCtrl(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, wxTextCtrl* searchBox) : wxListCtrl(parent, id, pos, size, wxLC_REPORT | wxLC_SINGLE_SEL | wxLC_VIRTUAL)
 {
-	RefreshProcesses();
+	m_imageList = new wxImageList(16, 16);
 	m_searchBox = searchBox;
+	HICON hDefApp;
+	HRESULT result = LoadIconWithScaleDown(NULL, MAKEINTRESOURCE(IDI_APPLICATION), 16, 16, &hDefApp);
+	if (result != S_OK)
+		LOG(1, L"Unable to load default app icon.");
+
+	m_defaultIcon.CreateFromHICON(WXHICON(hDefApp));
+
+	RefreshProcesses();
 	if (m_searchBox != nullptr)
 	{
 		m_searchBox->Bind(wxEVT_KEY_DOWN, &wxProcessListCtrl::OnSearchBoxType, this);
 		m_searchBox->Bind(wxEVT_KEY_UP, &wxProcessListCtrl::OnSearchBoxType, this);
 	}
+
 }
 
 unsigned long wxProcessListCtrl::GetSelectedProcessID()
 {
+	const std::vector<ProcessData>* list;
+	if (m_searchBox->GetValue().IsEmpty() || m_searchBox->GetValue().compare(L"Search") == 0)
+	{
+		list = &m_activeProcessList;
+	}
+	else {
+		list = &m_shownProcessList;
+	}
+
 	long itemId = this->GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
-	if (itemId > m_shownProcessList.size())
+	if (itemId > list->size())
 		return 0;
 
 	
-	wxString pidStr = OnGetItemText(itemId, 0);
+	wxString pidStr = OnGetItemText(itemId, 1);
 	unsigned long pid = 0;
 	pidStr.ToCULong(&pid, 10);
 	return pid;
+}
+
+int wxProcessListCtrl::OnGetItemImage(long item) const
+{
+	const std::vector<ProcessData>* list;
+	if (m_searchBox->GetValue().IsEmpty() || m_searchBox->GetValue().compare(L"Search") == 0)
+	{
+		list = &m_activeProcessList;
+	}
+	else {
+		list = &m_shownProcessList;
+	}
+
+	return list->at(item).hasIcon ? list->at(item).iconIndex : m_defaultIconIndex;
 }
 
 wxString wxProcessListCtrl::OnGetItemText(long item, long column) const
@@ -36,19 +68,19 @@ wxString wxProcessListCtrl::OnGetItemText(long item, long column) const
 		list = &m_shownProcessList;
 	}
 
-	if (item > list->size() || column > 2)
+	if (item > list->size() || column > 3)
 		return wxString();
 	
 	wxString string;
 	switch (column)
 	{
-	case 0:
+	case 1:
 		string << list->at(item).processID;
 		break;
-	case 1:
+	case 2:
 		string = list->at(item).windowName;
 		break;
-	case 2:
+	case 3:
 		string = list->at(item).szExeFile;
 	}
 	
@@ -60,14 +92,23 @@ void wxProcessListCtrl::RefreshProcesses()
 	if (m_searchBox != nullptr)
 		m_searchBox->Clear();
 
+	if (m_imageList != nullptr)
+	{
+		m_imageList->Destroy();
+		m_imageList->Create(16, 16);
+		m_defaultIconIndex = m_imageList->Add(m_defaultIcon);
+	}
+
 	this->ClearAll();
 
-	int pidWidth = 40;
+	int iconWidth = 30;
+	int pidWidth = 60;
 	int windowWidth = this->GetSize().GetWidth() / 2 - pidWidth;
 	int processWidth = this->GetSize().GetWidth() / 2 - pidWidth;
-	this->InsertColumn(0, _("PID"), wxLIST_FORMAT_LEFT, pidWidth);
-	this->InsertColumn(1, _("Window Name"), wxLIST_FORMAT_LEFT, windowWidth);
-	this->InsertColumn(2, _("Process"), wxLIST_FORMAT_LEFT, processWidth);
+	this->InsertColumn(0, _(""), wxLIST_FORMAT_CENTER, iconWidth);
+	this->InsertColumn(1, _("PID"), wxLIST_FORMAT_LEFT, pidWidth);
+	this->InsertColumn(2, _("Window Name"), wxLIST_FORMAT_LEFT, windowWidth);
+	this->InsertColumn(3, _("Process"), wxLIST_FORMAT_LEFT, processWidth);
 
 	m_activeProcessList.clear();
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -84,11 +125,31 @@ void wxProcessListCtrl::RefreshProcesses()
 			procData.processID = procEntry.th32ProcessID;
 			procData.windowName = nightshade::Utils::GetWindowTitleFromPID(procEntry.th32ProcessID);
 			procData.szExeFile = procEntry.szExeFile;
+			
+
+			if (wxString(procEntry.szExeFile).EndsWith(".exe"))
+			{
+				HICON hIcon = 0;
+				hIcon = nightshade::Utils::GetProcessIcon(procData.processID);
+
+				if (hIcon != NULL)
+				{
+					wxIcon wIcon;
+					wIcon.CreateFromHICON(WXHICON(hIcon));
+					wIcon = wIcon;
+					procData.iconIndex = m_imageList->Add(wIcon);
+					procData.hasIcon = true;
+					DestroyIcon(hIcon);
+				}
+				
+			}
+
 			m_activeProcessList.push_back(procData);
 		} while (Process32Next(hSnapshot, &procEntry));
 		CloseHandle(hSnapshot);
 	}
 	this->SetItemCount(m_activeProcessList.size());
+	this->SetImageList(m_imageList, wxIMAGE_LIST_SMALL);
 	this->Refresh();
 }
 
--- wxProcessListCtrl.h
+++ wxProcessListCtrl.h
@@ -3,11 +3,16 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <wx/listctrl.h>
+#include <wx/msw/private.h>
+#include <CommCtrl.h>
+#include "resource.h"
 
 struct ProcessData {
 	unsigned long processID;
 	std::wstring szExeFile;
 	std::wstring windowName;
+	bool hasIcon = false;
+	int iconIndex = 0;
 };
 
 class wxProcessListCtrl : public wxListCtrl
@@ -20,12 +25,17 @@ public:
 	void RefreshProcesses();
 	void OnSearchBoxType(wxKeyEvent& evt);
 
+
 private:
 	virtual wxString OnGetItemText(long item, long column) const wxOVERRIDE;
+	virtual int OnGetItemImage(long item) const wxOVERRIDE;
 
 private:
 	unsigned long m_selectedProcessID = 0;
 	std::vector<ProcessData> m_activeProcessList;
 	std::vector<ProcessData> m_shownProcessList;
 	wxTextCtrl* m_searchBox = nullptr;
+	wxImageList* m_imageList = nullptr;
+	wxIcon m_defaultIcon;
+	int m_defaultIconIndex = -1;
 };
